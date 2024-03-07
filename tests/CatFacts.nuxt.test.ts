12a6c933f941068f8becedb530fd0b94f68317f0
Douglas <yogi.yokomizo@gmail.com>
2024-03-07 07:36:06 -0300

    Delete tests directory
    tests are not working yet

--- tests/CatFacts.nuxt.test.ts
+++
@@ -1,9 +0,0 @@
-import { useCatFactStore } from "../stores/CatFactStore";
-
-describe("catFactStore", () => {
-  test("fetchCatFacts sets data", async () => {
-    const store = useCatFactStore();
-    await store.fetchCatFacts();
-    expect(Array.isArray(store.data)).toBeGreaterThan(0);
-  });
-});
